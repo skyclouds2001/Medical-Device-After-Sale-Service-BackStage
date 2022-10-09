bb0c101723f829bb7c153290655f884fabfd2184
skyclouds2001 <skyclouds2001@163.com>
2022-10-09 11:05:57 +0800

    feat: remove useless codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from '@/store'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 )
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setCounter } from '@/store/modules/main'
-import { RootStore } from '@/store'
+import React from 'react'
 import { PlusOutlined } from '@ant-design/icons'
 
 export default function Main(): JSX.Element {
-  const dispatch = useDispatch()
-
-  const { counter } = useSelector((store: RootStore) => store.main)
-
-  const [value, setValue] = useState(counter)
-
-  useEffect(() => {
-    console.log(counter)
-  }, [counter])
-
   return (
     <div>
       <span>Main</span>
       <PlusOutlined />
-      <div>
-        <input value={value} onChange={e => setValue(parseInt(e.target.value))} />
-        <button onClick={() => dispatch(setCounter({ counter: value }))}>保存</button>
-      </div>
     </div>
   )
 }
