59368a21983fbb638d48a0f5060a72282bb0d194
skyclouds2001 <skyclouds2001@163.com>
2022-10-09 12:02:37 +0800

    build: add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { ConfigProvider } from 'antd'
+import { ConfigProvider, Layout } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 import routes from './router'
 import './App.css'
 
+const { Header, Content, Footer } = Layout
+
 export default function App(): JSX.Element {
   return (
     <ConfigProvider locale={zhCN}>
-      <Routes>
-        {routes.map(v => {
-          return <Route key={v.path} path={v.path} element={v.component} />
-        })}
-      </Routes>
+      <Layout>
+        <Header>header</Header>
+        <Content>
+          <Routes>
+            {routes.map(v => {
+              return <Route key={v.path} path={v.path} element={v.component} />
+            })}
+          </Routes>
+        </Content>
+        <Footer>footer</Footer>
+      </Layout>
     </ConfigProvider>
   )
 }
