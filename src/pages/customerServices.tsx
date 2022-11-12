e16d19bc2795929d97263035139135089a54c274
skyclouds2001 <skyclouds2001@163.com>
2022-11-13 00:29:17 +0800

    feat: re-init project

--- src/pages/customerServices.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CustomerServices(): JSX.Element {
-  return (
-    <div>
-      <span>CustomerServices</span>
-    </div>
-  )
-}
--- src/pages/products.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Products(): JSX.Element {
-  return (
-    <div>
-      <span>Products</span>
-    </div>
-  )
-}
