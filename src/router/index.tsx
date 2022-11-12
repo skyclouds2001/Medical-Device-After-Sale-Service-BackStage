16149aa6cc9a589e94824479f23084de7f7491de
skyclouds2001 <skyclouds2001@163.com>
2022-11-13 00:29:52 +0800

    feat: re-init project

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,36 +1,10 @@
 import React from 'react'
-import Main from '@/pages/main'
-import Products from '@/pages/products'
-import CustomerServices from '@/pages/customerServices'
-import Acceptances from '@/pages/acceptances'
-import Clients from '@/pages/clients'
 
 interface Route {
   path: string
   component: JSX.Element
 }
 
-const routes: Route[] = [
-  {
-    path: '/',
-    component: <Main />
-  },
-  {
-    path: '/products',
-    component: <Products />
-  },
-  {
-    path: '/customer-services',
-    component: <CustomerServices />
-  },
-  {
-    path: '/acceptances',
-    component: <Acceptances />
-  },
-  {
-    path: '/clients',
-    component: <Clients />
-  }
-]
+const routes: Route[] = []
 
 export default routes
