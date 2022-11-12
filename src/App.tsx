d7f1b36c053508889be3354a19773d6a3a399558
skyclouds2001 <skyclouds2001@163.com>
2022-11-13 00:38:05 +0800

    feat: small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { ConfigProvider, Layout } from 'antd'
+import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
 import routes from './router'
 import './App.css'
 
-const { Header, Content, Footer } = Layout
-
 export default function App(): JSX.Element {
   return (
     <ConfigProvider locale={zhCN}>
-      <Layout>
-        <Header>header</Header>
-        <Content>
-          <Routes>
-            {routes.map(v => {
-              return <Route key={v.path} path={v.path} element={v.component} />
-            })}
-          </Routes>
-        </Content>
-        <Footer>footer</Footer>
-      </Layout>
+      <Routes>
+        {routes.map(v => {
+          return <Route key={v.path} path={v.path} element={v.component} />
+        })}
+      </Routes>
     </ConfigProvider>
   )
 }
