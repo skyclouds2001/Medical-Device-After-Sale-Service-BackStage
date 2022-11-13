024e4cce04caf81ee4cc0d24c81ee0465537041f
skyclouds2001 <skyclouds2001@163.com>
2022-11-13 10:13:13 +0800

    build: small changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,10 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'standard-with-typescript',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended'
   ],
   overrides: [],
