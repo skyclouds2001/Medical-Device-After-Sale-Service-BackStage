06428294f340c64439abf8f38fed8d7fbdbf2c8a
skyclouds2001 <skyclouds2001@163.com>
2022-11-24 01:02:00 +0800

    feat: update style use tailwindcss

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -19,73 +19,32 @@ export default function LoginPage(): JSX.Element {
   return (
     <>
       {/* eslint-disable @typescript-eslint/no-unsafe-assignment */}
-      <div
-        style={{
-          position: 'fixed',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          width: '100vw',
-          height: '100vh',
-          margin: '0 auto',
-          padding: 0
-        }}
-      >
-        <img
-          src={bgImg}
-          alt=""
-          style={{
-            objectFit: 'cover',
-            objectPosition: 'center',
-            width: '100%',
-            height: '100%'
-          }}
-        />
+      <div className="fixed inset-0 mx-auto my-0 p-0 w-screen h-screen">
+        <img src={bgImg} alt="" className="object-cover object-center w-full h-full" />
       </div>
       <div
+        className="fixed inset-0 p-6 mx-auto my-48 w-96 h-80 bg-white opacity-75 rounded-lg"
         style={{
-          position: 'fixed',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-          margin: '25vh auto',
-          padding: '25px',
-          width: '400px',
-          height: '300px',
-          backgroundColor: 'rgba(255, 255, 255, 0.75)',
-          borderRadius: '10px',
           boxShadow: '5px 5px 10px rgba(255, 255, 255, 0.5)'
         }}
       >
         <div
+          className="text-xl tracking-widest cursor-default select-none"
           style={{
-            fontSize: '20px',
-            letterSpacing: '2px',
             color: 'rgb(15, 15, 65)'
           }}
         >
           登录
         </div>
-        <div
-          style={{
-            margin: '20px 0'
-          }}
-        >
+        <div className="mx-0 my-5">
           <Input placeholder="请输入用户名" prefix={<IdcardFilled />} value={user} onChange={e => setUser(e.target.value)} />
         </div>
-        <div
-          style={{
-            margin: '10px 0'
-          }}
-        >
+        <div className="mx-0 my-2.5">
           <Input.Password placeholder="请输入密码" prefix={<LockFilled />} value={password} onChange={e => setPassword(e.target.value)} />
         </div>
         <div
+          className="mx-0 my-2.5 text-left"
           style={{
-            margin: '10px 0',
-            textAlign: 'left',
             color: 'rgb(122, 121, 130)'
           }}
         >
@@ -93,20 +52,12 @@ export default function LoginPage(): JSX.Element {
             记住密码
           </Checkbox>
         </div>
-        <div
-          style={{
-            margin: '20px 0 10px'
-          }}
-        >
+        <div className="mt-5 mb-2.5 mx-0">
           <button
+            className="w-40 text-lg text-white border-none focus:outline-none"
             onClick={handleLogin}
             style={{
-              width: '10em',
-              backgroundImage: 'linear-gradient(135deg, rgb(70, 100, 230), rgb(70, 100, 190) 50%, rgb(40, 50, 150))',
-              color: 'white',
-              fontSize: '18px',
-              border: 'none',
-              outline: 'none'
+              backgroundImage: 'linear-gradient(135deg, rgb(70, 100, 230), rgb(70, 100, 190) 50%, rgb(40, 50, 150))'
             }}
           >
             登录
@@ -114,10 +65,10 @@ export default function LoginPage(): JSX.Element {
         </div>
         <div>
           <p
+            className="text-right cursor-pointer select-none"
             onClick={handleForgetPassword}
             style={{
-              color: 'rgb(86, 107, 217)',
-              textAlign: 'right'
+              color: 'rgb(86, 107, 217)'
             }}
           >
             忘记密码？
