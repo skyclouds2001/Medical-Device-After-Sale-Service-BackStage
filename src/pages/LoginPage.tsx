7662f5a680230683e7749243e36e6f9eb530f970
skyclouds2001 <skyclouds2001@163.com>
2022-11-28 23:55:47 +0800

    feat: update login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,26 +1,71 @@
-import React, { useState } from 'react'
-import { Input, Checkbox } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { Input, Checkbox, message } from 'antd'
 import { IdcardFilled, LockFilled } from '@ant-design/icons'
+import { adminLogin } from '@/apis'
+import Storage from '@/utils/storage'
 import bgImg from '@/assets/bg-img-login.png'
 
+interface LoginStorageConfig {
+  user: string
+  password?: string
+  remember: boolean
+}
+
 export default function LoginPage(): JSX.Element {
+  const [messageApi, contextHolder] = message.useMessage()
+
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
   const [remember, setRemember] = useState(false)
 
+  useEffect(() => {
+    const configs = Storage.getItem<LoginStorageConfig>('login')
+    if (configs != null) {
+      setUser(configs.user)
+      setPassword(configs.password ?? '')
+      setRemember(configs.remember)
+    }
+  })
+
+  /**
+   * 执行登录操作方法
+   */
   const handleLogin = (): void => {
-    console.log('login')
+    adminLogin(user, password)
+      .then(res => {
+        console.log(res) // todo
+        if (res.code === 0) {
+          void messageApi.success({
+            content: '登录成功'
+          })
+          Storage.setItem('login', {
+            user,
+            password: remember ? password : undefined,
+            remember
+          })
+        } else {
+          void messageApi.error({
+            content: res.data
+          })
+        }
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
+  /**
+   * 执行找回密码操作方法 todo
+   */
   const handleForgetPassword = (): void => {
     console.log('forget')
   }
 
   return (
     <>
-      {/* eslint-disable @typescript-eslint/no-unsafe-assignment */}
+      {contextHolder}
       <div className="fixed inset-0 mx-auto my-0 p-0 w-screen h-screen">
-        <img src={bgImg} alt="" className="object-cover object-center w-full h-full" />
+        <img src={bgImg as string} alt="" className="object-cover object-center w-full h-full" />
       </div>
       <div
         className="fixed inset-0 p-6 mx-auto my-48 w-96 h-80 bg-white opacity-75 rounded-lg"
