929e1537d15285f64b05cec430e95d635cb4ca8d
skyclouds2001 <skyclouds2001@163.com>
2022-11-29 14:33:39 +0800

    feat: support service auto add login configs

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { BASE_URL, NETWORK_TIMEOUT } from '@/config'
+import Storage from '@/utils/storage'
 
 const instance = axios.create({
   baseURL: BASE_URL,
@@ -7,7 +8,15 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use(
-  config => config,
+  config => {
+    const token = Storage.getItem<string>('token')
+    if (token != null && config.url != null && !['/wizz/aftersale/account/admin/login', '/wizz/aftersale/account/admin/resetPassword'].includes(config.url)) {
+      config.headers = {
+        Authorization: token
+      }
+    }
+    return config
+  },
   async error => await Promise.reject(error)
 )
 
