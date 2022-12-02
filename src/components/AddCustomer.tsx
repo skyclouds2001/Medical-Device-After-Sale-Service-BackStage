750b0c3126883222d20c14bd1801590cb9727225
skyclouds2001 <skyclouds2001@163.com>
2022-12-02 14:45:21 +0800

    feat: change to select company

--- src/components/AddCustomer.tsx
+++ src/components/AddCustomer.tsx
@@ -1,5 +1,7 @@
-import React, { useState, forwardRef, useImperativeHandle, useRef } from 'react'
-import { Form, Input } from 'antd'
+import React, { useState, forwardRef, useImperativeHandle, useRef, useEffect } from 'react'
+import { Form, Input, Select } from 'antd'
+import { getCompanyInfo } from '@/apis'
+import type Company from '@/model/company'
 
 interface AddCustomerProps {
   crf: ReturnType<typeof useRef>
@@ -9,6 +11,7 @@ export default forwardRef(function AddCustomer(props: AddCustomerProps, _ref): J
   const [name, setName] = useState('')
   const [mobile, setMobile] = useState('')
   const [company, setCompany] = useState(0)
+  const [companies, setCompanies] = useState<Company[]>([])
 
   useImperativeHandle(props.crf, () => ({
     getCustomer: () => ({
@@ -18,16 +21,42 @@ export default forwardRef(function AddCustomer(props: AddCustomerProps, _ref): J
     })
   }))
 
+  useEffect(() => {
+    const loadCompany = async (): Promise<void> => {
+      const res = await getCompanyInfo(true, 1)
+      setCompanies([...res.data.company_list])
+      const num = res.data.total_page_num
+      const pros = []
+      for (let i = 2; i <= num; ++i) {
+        pros.push(getCompanyInfo(false, i))
+      }
+      const result = await Promise.allSettled(pros)
+      result.forEach(v => {
+        if (v.status !== 'rejected') {
+          setCompanies([...companies, ...v.value.data.company_list])
+        }
+      })
+    }
+
+    void loadCompany()
+  }, [])
+
   return (
     <Form labelCol={{ span: 8 }} colon={false}>
       <Form.Item label="客户名称" name="name">
-        <Input className="rounded-xl mx-2" placeholder="请输入客户名称" value={name} onChange={e => setName(e.target.value)} />
+        <Input className="rounded-sm mx-2" placeholder="请输入客户名称" value={name} onChange={e => setName(e.target.value)} />
       </Form.Item>
       <Form.Item label="客户联系方式" name="mobile">
-        <Input className="rounded-xl mx-2" placeholder="请输入客户联系方式" value={mobile} onChange={e => setMobile(e.target.value)} />
+        <Input className="rounded-sm mx-2" placeholder="请输入客户联系方式" value={mobile} onChange={e => setMobile(e.target.value)} />
       </Form.Item>
       <Form.Item label="客户所属公司" name="company">
-        <Input className="rounded-xl mx-2" placeholder="请输入客户所属公司" value={company} onChange={e => setCompany(parseInt(e.target.value))} />
+        <Select className="rounded-sm mx-2" placeholder="请选择客户所属公司" onChange={(value: number) => setCompany(value)}>
+          {companies.map(v => (
+            <Select.Option key={v.company_id} value={v.company_id}>
+              {v.company_name}
+            </Select.Option>
+          ))}
+        </Select>
       </Form.Item>
     </Form>
   )
--- src/components/EditCustomer.tsx
+++ src/components/EditCustomer.tsx
@@ -1,6 +1,8 @@
-import React, { useState, forwardRef, useImperativeHandle, useRef } from 'react'
-import { Form, Input } from 'antd'
+import React, { useState, forwardRef, useImperativeHandle, useRef, useEffect } from 'react'
+import { Form, Input, Select } from 'antd'
 import type Customer from '@/model/customer'
+import { getCompanyInfo } from '@/apis'
+import type Company from '@/model/company'
 
 interface EditCustomerProps {
   customer: Customer
@@ -11,6 +13,7 @@ export default forwardRef(function EditCustomer(props: EditCustomerProps, _ref):
   const [name, setName] = useState('')
   const [mobile, setMobile] = useState('')
   const [company, setCompany] = useState(0)
+  const [companies, setCompanies] = useState<Company[]>([])
 
   useImperativeHandle(props.crf, () => ({
     getCustomer: () => ({
@@ -19,6 +22,26 @@ export default forwardRef(function EditCustomer(props: EditCustomerProps, _ref):
       company
     })
   }))
+
+  useEffect(() => {
+    const loadCompany = async (): Promise<void> => {
+      const res = await getCompanyInfo(true, 1)
+      setCompanies([...res.data.company_list])
+      const num = res.data.total_page_num
+      const pros = []
+      for (let i = 2; i <= num; ++i) {
+        pros.push(getCompanyInfo(false, i))
+      }
+      const result = await Promise.allSettled(pros)
+      result.forEach(v => {
+        if (v.status !== 'rejected') {
+          setCompanies([...companies, ...v.value.data.company_list])
+        }
+      })
+    }
+
+    void loadCompany()
+  }, [])
 
   return (
     <Form labelCol={{ span: 8 }} colon={false}>
@@ -29,7 +52,13 @@ export default forwardRef(function EditCustomer(props: EditCustomerProps, _ref):
         <Input className="rounded-xl mx-2" placeholder="请输入客户联系方式" value={mobile} onChange={e => setMobile(e.target.value)} />
       </Form.Item>
       <Form.Item label="客户所属公司" name="company">
-        <Input className="rounded-xl mx-2" placeholder="请输入客户所属公司" value={company} onChange={e => setCompany(parseInt(e.target.value))} />
+        <Select className="rounded-sm mx-2" placeholder="请选择客户所属公司" onChange={(value: number) => setCompany(value)}>
+          {companies.map(v => (
+            <Select.Option key={v.company_id} value={v.company_id}>
+              {v.company_name}
+            </Select.Option>
+          ))}
+        </Select>
       </Form.Item>
     </Form>
   )
