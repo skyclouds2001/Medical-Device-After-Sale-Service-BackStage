909376c25772c758eff19adab0f1d232e127530e
skyclouds2001 <skyclouds2001@163.com>
2022-12-04 22:12:56 +0800

    feat: add company manage page

--- src/pages/CompanyManage.tsx
+++ src/pages/CompanyManage.tsx
@@ -1,49 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { message, Table, Button, Modal } from 'antd'
-import { getCustomerInfo, updateCustomerInfo, removeCustomerInfo, addCustomerInfo } from '@/apis'
+import { getCompanyInfo, addCompanyInfo, updateCompanyInfo, removeCompanyInfo } from '@/apis'
 import { DEFAULT_PAGE_SIZE } from '@/config'
-import AddCustomer from '@/components/AddCustomer'
-import EditCustomer from '@/components/EditCustomer'
-import type Customer from '@/model/customer'
+import type Company from '@/model/company'
 
 const { Column } = Table
 
 export default function CompanyManage(): JSX.Element {
   const [messageApi, contextHolder] = message.useMessage()
 
-  /** 添加客户表单 ref 引用 */
-  const addRef = useRef<ReturnType<typeof AddCustomer>>(null)
-  /** 编辑客户表单 ref 引用 */
-  const editRef = useRef<ReturnType<typeof EditCustomer>>(null)
-
-  /** 客户列表 */
-  const [customers, setCustomers] = useState<Customer[]>([])
-  /** 客户总数 */
+  /** 企业列表 */
+  const [company, setCompany] = useState<Company[]>([])
+  /** 企业总数 */
   const [total, setTotal] = useState(0)
-  /** 标记客户列表是否处于加载状态 */
+  /** 标记企业列表是否处于加载状态 */
   const [isLoading, setLoading] = useState(false)
-  /** 客户列表当前页数 */
+  /** 企业列表当前页数 */
   const [pageNum, setPageNum] = useState(1)
 
   useEffect(() => {
-    void loadCustomer(true, 1)
+    void loadCompany(true, 1)
   }, [])
 
   useEffect(() => {
-    void loadCustomer(false, pageNum)
+    void loadCompany(false, pageNum)
   }, [pageNum])
 
   /**
-   * 加载客户信息
+   * 加载企业信息
    * @param isFirst 是否首次加载
    * @param num 当前页数
    */
-  const loadCustomer = async (isFirst: boolean, num: number): Promise<void> => {
+  const loadCompany = async (isFirst: boolean, num: number): Promise<void> => {
     setLoading(true)
     try {
-      const res = await getCustomerInfo(isFirst, num)
+      const res = await getCompanyInfo(isFirst, num)
       if (res.code === 0) {
-        setCustomers(res.data.customer_list)
+        setCompany(res.data.company_list)
         if (isFirst) setTotal(res.data.total_num)
       } else {
         void messageApi.error({
@@ -59,29 +52,24 @@ export default function CompanyManage(): JSX.Element {
   }
 
   /**
-   * 添加客户
+   * 添加企业
    */
-  const addCustomer = (): void => {
+  const addCompany = (): void => {
     Modal.confirm({
-      title: '添加客户信息',
-      content: <AddCustomer ref={addRef} crf={addRef} />,
+      title: '添加企业信息',
+      content: '', // todo
       closable: true,
       okButtonProps: {
         className: 'text-blue-500'
       },
       onOk: async () => {
-        const cus = (
-          addRef.current as unknown as {
-            getCustomer: () => { name: string; mobile: string; company: number }
-          }
-        ).getCustomer()
         try {
-          const res = await addCustomerInfo(cus.company, cus.name, cus.mobile)
+          const res = await addCompanyInfo('') // todo
           if (res.code === 0) {
             void messageApi.success({
-              content: '更新成功'
+              content: '添加成功'
             })
-            void loadCustomer(false, pageNum)
+            void loadCompany(false, pageNum)
           } else {
             void messageApi.error({
               content: res.data
@@ -95,30 +83,25 @@ export default function CompanyManage(): JSX.Element {
   }
 
   /**
-   * 更新客户信息
-   * @param customer 需更新的客户信息
+   * 更新企业信息
+   * @param company 需更新的企业信息
    */
-  const editCustomer = (customer: Customer): void => {
+  const editCompany = (company: Company): void => {
     Modal.confirm({
-      title: '修改客户信息',
-      content: <EditCustomer ref={editRef} crf={editRef} customer={customer} />,
+      title: '修改企业信息',
+      content: '', // todo
       closable: true,
       okButtonProps: {
         className: 'text-blue-500'
       },
       onOk: async () => {
-        const cus = (
-          editRef.current as unknown as {
-            getCustomer: () => { name: string; mobile: string; company: number }
-          }
-        ).getCustomer()
         try {
-          const res = await updateCustomerInfo(cus.company, customer.customer_id, cus.name, cus.mobile)
+          const res = await updateCompanyInfo(0, '') // todo
           if (res.code === 0) {
             void messageApi.success({
               content: '更新成功'
             })
-            void loadCustomer(false, pageNum)
+            void loadCompany(false, pageNum)
           } else {
             void messageApi.error({
               content: res.data
@@ -132,23 +115,23 @@ export default function CompanyManage(): JSX.Element {
   }
 
   /**
-   * 移除客户信息
-   * @param customer 客户信息
+   * 移除企业信息
+   * @param company 企业信息
    */
-  const removeCustomer = (customer: Customer): void => {
+  const removeCompany = (company: Company): void => {
     Modal.confirm({
       title: '警告',
-      content: '确认移除客户信息？',
+      content: '确认移除企业信息？',
       okText: '删除',
       okType: 'danger',
       closable: true,
       onOk: async () => {
-        const res = await removeCustomerInfo(customer.customer_id)
+        const res = await removeCompanyInfo(company.company_id)
         if (res.code === 0) {
           void messageApi.success({
             content: '删除成功'
           })
-          void loadCustomer(false, pageNum)
+          void loadCompany(false, pageNum)
         } else {
           void messageApi.error({
             content: res.data
@@ -163,18 +146,18 @@ export default function CompanyManage(): JSX.Element {
       {/* AntD Message 动态组件 */}
       {contextHolder}
 
-      {/* 添加客户信息按钮区域 */}
+      {/* 添加企业信息按钮区域 */}
       <div className="my-5 text-right">
-        <Button className="text-blue-500" type="primary" onClick={() => addCustomer()}>
-          添加客户
+        <Button className="text-blue-500" type="primary" onClick={() => addCompany()}>
+          添加企业
         </Button>
       </div>
 
-      {/* 客户信息表格 */}
+      {/* 企业信息表格 */}
       <Table
-        dataSource={customers}
+        dataSource={company}
         bordered
-        rowKey="customer_id"
+        rowKey="company_id"
         loading={isLoading}
         pagination={{
           current: pageNum,
@@ -183,19 +166,17 @@ export default function CompanyManage(): JSX.Element {
         }}
         onChange={pagination => setPageNum(pagination.current ?? 1)}
       >
-        <Column align="center" title="用户名称" dataIndex="customer_name" key="customer_name" />
         <Column align="center" title="企业名称" dataIndex="company_name" key="company_name" />
-        <Column align="center" title="联系方式" dataIndex="mobile" key="mobile" />
         <Column
           align="center"
           title="操作"
           key="action"
-          render={(_, record: Customer) => (
+          render={(_, record: Company) => (
             <>
-              <Button type="link" onClick={() => editCustomer(record)}>
+              <Button type="link" onClick={() => editCompany(record)}>
                 编辑
               </Button>
-              <Button type="link" danger onClick={() => removeCustomer(record)}>
+              <Button type="link" danger onClick={() => removeCompany(record)}>
                 删除
               </Button>
             </>
