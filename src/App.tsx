420b307ed98978d2da90436988755362360cfd30
skyclouds2001 <skyclouds2001@163.com>
2023-01-03 19:53:46 +0800

    feat: update dayjs locate

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 import Routes from '@/router'
 import '@/App.css'
+
+dayjs.locale('zh-cn')
 
 export default function App(): JSX.Element {
   return (
