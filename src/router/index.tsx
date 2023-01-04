1b24dd37334a6d6533a46496eda645378707fb25
skyclouds2001 <skyclouds2001@163.com>
2023-01-04 14:11:50 +0800

    feat: rename folder from pages to views

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom'
-import BasePage from '@/pages/BasePage'
-import LoginPage from '@/pages/LoginPage'
-import ProductManage from '@/pages/ProductManage'
-import UserManage from '@/pages/UserManage'
-import CustomerServiceManage from '@/pages/CustomerServiceManage'
-import CompanyManage from '@/pages/CompanyManage'
-import CustomerManage from '@/pages/CustomerManage'
+import BasePage from '@/views/BasePage'
+import LoginPage from '@/views/LoginPage'
+import ProductManage from '@/views/ProductManage'
+import UserManage from '@/views/UserManage'
+import CustomerServiceManage from '@/views/CustomerServiceManage'
+import CompanyManage from '@/views/CompanyManage'
+import CustomerManage from '@/views/CustomerManage'
 
 const Routes: React.FC = () => {
   return useRoutes([
