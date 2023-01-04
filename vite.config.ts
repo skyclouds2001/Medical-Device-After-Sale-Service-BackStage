8de4dafb04099af7b13a090ef8193fe9a6539715
skyclouds2001 <skyclouds2001@163.com>
2023-01-05 00:52:33 +0800

    build: remove plugin vite-plugin-stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
-import stylelint from 'vite-plugin-stylelint'
 
 export default defineConfig({
-  plugins: [react(), legacy(), eslint(), stylelint()],
+  plugins: [react(), legacy(), eslint()],
   css: {
     postcss: '.postcssrc.js',
     devSourcemap: true
