a95ec01eecfa9fc8ea821632240c8e4517c011bd
skyclouds2001 <skyclouds2001@163.com>
2023-01-05 18:21:16 +0800

    fix: project bug

--- src/network/index.ts
+++ src/network/index.ts
@@ -14,7 +14,7 @@ instance.interceptors.request.use(
     if (!WHITE_LIST.includes(config.url ?? '')) {
       config.headers = {
         ...config.headers,
-        Authorization: Storage.getStorage('token') ?? ''
+        Authorization: Storage.getStorage('token') as string
       } as unknown as AxiosHeaders
     }
     return config
