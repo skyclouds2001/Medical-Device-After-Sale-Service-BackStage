9d08bafb9485b88bbfdf87bfcc0b744ec431a90d
skyclouds2001 <skyclouds2001@163.com>
2023-01-05 20:15:21 +0800

    build: import rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 import eslint from 'vite-plugin-eslint'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react(), legacy(), eslint()],
+  plugins: [react(), legacy(), eslint(), visualizer()],
   css: {
     postcss: '.postcssrc.js',
     devSourcemap: true
