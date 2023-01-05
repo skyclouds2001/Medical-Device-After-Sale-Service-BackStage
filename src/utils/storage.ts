4f890dbc6da29a37657d8ee39ad8579754fe89cc
skyclouds2001 <skyclouds2001@163.com>
2023-01-05 18:16:43 +0800

    feat: use storage encrypt

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,10 @@
+/* eslint-disable */
 import { lowerCase } from 'lodash'
+import CryptoJS from 'crypto-js'
 import { APPLICATION_NAME, APPLICATION_VERSION } from '@/config'
+
+const SECRET_KEY = CryptoJS.enc.Utf8.parse('3333e6e143439161')
+const SECRET_IV = CryptoJS.enc.Utf8.parse('e3bbe7e3ba84431a')
 
 const prefix = lowerCase(APPLICATION_NAME.replace(/[a-z]/g, '')) + '_' + APPLICATION_VERSION
 const storage = window.localStorage
@@ -11,11 +16,23 @@ interface Data {
 }
 
 const encrypt = (data: string): string => {
-  return data
+  return CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(data), SECRET_KEY, {
+    iv: SECRET_IV,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  }).ciphertext.toString()
 }
 
 const decrypt = (data: string): string => {
-  return data
+  return CryptoJS.AES.decrypt(
+    CryptoJS.enc.Base64.stringify(CryptoJS.enc.Hex.parse(data)),
+    SECRET_KEY,
+    {
+      iv: SECRET_IV,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    }
+  ).toString(CryptoJS.enc.Utf8).toString()
 }
 
 export const getStorage = (key: string): any => {
