9d00d7396f8e087d6c93ab7525073920ebc9a09d
skyclouds2001 <skyclouds2001@163.com>
2023-01-15 15:33:36 +0800

    feat: update config file comment

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,14 +10,14 @@ export const BASE_URL = 'http://aftersale.divergentcloud.com'
 /** 默认网络超时时间 */
 export const NETWORK_TIMEOUT = 10000
 
-/** 默认请求无需token接口名单，若修改需同时修改后端 */
+/** 默认请求无需token接口名单 !!!若修改需同时修改后端!!! */
 export const WHITE_LIST = ['/wizz/aftersale/account/admin/login', '/wizz/aftersale/account/admin/resetPassword']
 
 /** 默认重定向路径 - 登录后默认跳转的路径 */
 export const DEFAULT_REDIRECT_PATH = '/product'
 
-/** 默认单页的容量，若修改需同时修改后端 */
+/** 默认单页的容量 !!!若修改需同时修改后端!!! */
 export const DEFAULT_PAGE_SIZE = 10
 
-/** 默认登录状态过期时间，若修改需同时修改后端 */
+/** 默认登录状态过期时间 !!!若修改需同时修改后端!!! */
 export const SESSION_EXPIRE = 3 * 24 * 60 * 60 * 1000
