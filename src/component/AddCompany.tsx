61724f5c6fd129f71677ca863b393795b8e932c0
skyclouds2001 <skyclouds2001@163.com>
2023-01-17 11:51:59 +0800

    feat: update company manage

---
+++ src/component/AddCompany.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react'
+import { Form, Input, Modal } from 'antd'
+import type { InputRef } from 'antd'
+import type { Company } from '@/model'
+
+interface AddCompanyProps {
+  open: boolean
+  onSubmit: (props: Omit<Company, 'company_id'>) => void
+  onCancel: () => void
+}
+
+const AddCompany: React.FC<AddCompanyProps> = props => {
+  const name = useRef<InputRef>(null)
+
+  const submit = (): void => {
+    props.onSubmit({
+      company_name: name.current?.input?.value ?? '',
+    })
+  }
+
+  const cancel = (): void => {
+    props.onCancel()
+  }
+
+  return (
+    <Modal open={props.open} title="添加企业信息" closable={true} onOk={submit} onCancel={cancel}>
+      <Form labelCol={{ span: 8 }} colon={false}>
+        <Form.Item label="企业名称" name="name">
+          <Input ref={name} className="rounded-xl mx-2" autoComplete="off" placeholder="请输入企业名称" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default AddCompany
---
+++ src/component/EditCompany.tsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+import { Form, Input, Modal } from 'antd'
+import type { InputRef } from 'antd'
+import type { Company } from '@/model'
+
+interface EditCompanyProps {
+  open: boolean
+  onSubmit: (props: Company) => void
+  onCancel: () => void
+  properties: Company
+}
+
+const EditCompany: React.FC<EditCompanyProps> = props => {
+  const name = useRef<InputRef>(null)
+
+  const submit = (): void => {
+    props.onSubmit({
+      company_name: name.current?.input?.value ?? props.properties.company_name,
+      company_id: props.properties.company_id,
+    })
+  }
+
+  const cancel = (): void => {
+    props.onCancel()
+  }
+
+  return (
+    <Modal open={props.open} title="修改企业信息" closable={true} onOk={submit} onCancel={cancel}>
+      <Form labelCol={{ span: 8 }} colon={false}>
+        <Form.Item label="企业名称" name="name">
+          <Input ref={name} className="rounded-xl mx-2" autoComplete="off" placeholder="请输入企业名称" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default EditCompany
