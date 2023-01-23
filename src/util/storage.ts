1ecdd8c99a4aa87e28b3d3a845658f27e7dd8ba2
skyclouds2001 <skyclouds2001@163.com>
2023-01-23 17:57:31 +0800

    feat: add comments

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -3,15 +3,28 @@ import { lowerCase } from 'lodash-es'
 import { APPLICATION_NAME, APPLICATION_VERSION } from '@/config'
 import { encrypt, decrypt } from '@/util'
 
+/** 存储键名前缀 */
 const prefix = lowerCase(APPLICATION_NAME.replace(/[a-z]/g, '')) + '_' + APPLICATION_VERSION
+/** 默认存储类型 */
 const storage = window.localStorage
 
-interface Data {
+/**
+ * 存储数据的数据格式
+ */
+interface Data <T = any> {
+  /** 数据内容 */
   value: any
+  /** 存储时间戳 */
   time: number
+  /** 超时时间长度 */
   expire: number
 }
 
+/**
+ * 获取存储方法
+ *
+ * @param {string} key 键名
+ */
 export const getStorage = <T = unknown>(key: string): T | null => {
   const s_key = prefix + '_' + key
 
@@ -19,7 +32,7 @@ export const getStorage = <T = unknown>(key: string): T | null => {
     return null
   }
 
-  const data: Data = JSON.parse(decrypt(storage.getItem(s_key) as string))
+  const data: Data<T> = JSON.parse(decrypt(storage.getItem(s_key) as string))
 
   if (data.expire + data.time > Date.now()) {
     return data.value
@@ -29,6 +42,13 @@ export const getStorage = <T = unknown>(key: string): T | null => {
   }
 }
 
+/**
+ * 设置存储方法
+ *
+ * @param {string} key 键名
+ * @param {any} value 键值
+ * @param {number} expire 超时时间
+ */
 export const setStorage = <T = unknown>(key: string, value: T | null, expire = 0): void => {
   const s_key = prefix + '_' + key
   if (value === null || value === undefined) {
@@ -49,13 +69,24 @@ export const setStorage = <T = unknown>(key: string, value: T | null, expire = 0
   storage.setItem(s_key, data)
 }
 
+/**
+ * 移除存储方法
+ *
+ * @param {string} key 键名
+ */
 export const removeStorage = (key: string): void => {
   const s_key = prefix + '_' + key
   storage.removeItem(s_key)
 }
 
+/**
+ * 清空存储方法
+ */
 export const clearStorage = (): void => {
   storage.clear()
 }
 
+/**
+ * 返回当前存储长度
+ */
 export const length = (): number => storage.length
