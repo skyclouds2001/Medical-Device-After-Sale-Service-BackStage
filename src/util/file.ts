5a203623830aaf754879cc1586b97e4c70d27e83
skyclouds2001 <skyclouds2001@163.com>
2023-03-16 19:23:51 +0800

    feat: add get file type util method

--- src/util/file.ts
+++ src/util/file.ts
@@ -21,3 +21,27 @@ export const getImageBase64 = (img: File): Promise<string> => {
     })
   })
 }
+
+type FileType = 'unknown' | 'DOCX' | 'XLSX' | 'PPTX' | 'PDF' | 'ZIP'
+
+/**
+ * 根据文件URL返回文件类型方法
+ *
+ * @param url 文件URL
+ * @returns 文件类型
+ */
+export const getFileType = (url: string): FileType => {
+  let type: FileType = 'unknown'
+  if (url.includes('.docx')) {
+    type = 'DOCX'
+  } else if (url.includes('.xlsx')) {
+    type = 'XLSX'
+  } else if (url.includes('.pptx')) {
+    type = 'PPTX'
+  } else if (url.includes('.pdf')) {
+    type = 'PDF'
+  } else if (url.includes('.zip')) {
+    type = 'ZIP'
+  }
+  return type
+}
