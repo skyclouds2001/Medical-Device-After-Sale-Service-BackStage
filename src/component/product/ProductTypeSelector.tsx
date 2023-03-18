7a24f0f3c1879119bbb7b2a0197ccddb6f4bdeb7
skyclouds2001 <skyclouds2001@163.com>
2023-03-18 17:31:14 +0800

    feat: update product type selector component

--- src/component/product/ProductTypeSelector.tsx
+++ src/component/product/ProductTypeSelector.tsx
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { Select, App } from 'antd'
+import React from 'react'
+import { Select } from 'antd'
+import useSwr from 'swr'
 import { getAllProductTypes } from '@/api'
-import type { ProductType } from '@/model'
 
 interface ProductTypeSelectorProps {
   onSelect: (value: number) => void
 }
 
 const ProductTypeSelector: React.FC<ProductTypeSelectorProps> = props => {
-  const { message } = App.useApp()
-
-  /** 产品大类列表 */
-  const [productTypes, setProductTypes] = useState<ProductType[]>([])
-
-  useEffect(() => {
-    const loadProductTypes = async (): Promise<void> => {
-      try {
-        const res = await getAllProductTypes()
-        if (res.code === 0) {
-          setProductTypes(res.data)
-        } else {
-          void message.error({
-            content: res.data.toString(),
-          })
-        }
-      } catch (err) {
-        console.error(err)
-      }
-    }
-
-    void loadProductTypes()
-  }, [])
+  const { data } = useSwr('/wizz/aftersale/product-model/getByTypeId', getAllProductTypes)
 
   return (
     <Select className="rounded-sm mx-2" placeholder="请选择产品所属大类" onSelect={props.onSelect}>
-      {productTypes.map(v => (
+      {(data?.data ?? []).map(v => (
         <Select.Option key={v.type_id} value={v.type_id}>
           {v.type_name}
         </Select.Option>
