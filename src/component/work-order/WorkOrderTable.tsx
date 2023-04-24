8c2ffcfdcfdebd99312b72b0ae898f699c747664
skyclouds2001 <skyclouds2001@163.com>
2023-04-24 19:08:26 +0800

    fix: text change

--- src/component/work-order/WorkOrderTable.tsx
+++ src/component/work-order/WorkOrderTable.tsx
@@ -34,7 +34,7 @@ const WorkOrderTable: React.FC<WorkOrderTableProps> = props => {
   return (
     <>
       <Table dataSource={props.workOrders} bordered rowKey="order_id" loading={props.loading} pagination={{ hideOnSinglePage: true }} expandable={{ expandedRowRender: record => <WorkOrderTableExpandable order={record} />, expandIcon: ({ expanded, onExpand, record }) => (expanded ? <DownOutlined onClick={e => onExpand(record, e)} /> : <RightOutlined onClick={e => onExpand(record, e)} />), columnTitle: '工单详情', columnWidth: '200px' }}>
-        <Table.Column width="200px" align="center" title="用户名" dataIndex="customer_name" key="customer_name" />
+        <Table.Column width="200px" align="center" title="客户名称" dataIndex="customer_name" key="customer_name" />
         <Table.Column width="200px" align="center" title="所属企业" dataIndex="customer_company" key="customer_company" />
         <Table.Column width="200px" align="center" title="产品名称" dataIndex="model_name" key="model_name" />
         <Table.Column width="150px" align="center" title="服务类型" dataIndex="order_type" key="order_type" render={(_, record: WorkOrder) => <>{services.find(v => v.id === record.order_type)?.text}</>} />
