dc2eab071fafa3a346791a791121efacb6266d45
skyclouds2001 <skyclouds2001@163.com>
2023-06-03 23:06:10 +0800

    feat: update work order customer information show

--- src/component/work-order/WorkOrderTable.tsx
+++ src/component/work-order/WorkOrderTable.tsx
@@ -34,8 +34,19 @@ const WorkOrderTable: React.FC<WorkOrderTableProps> = props => {
   return (
     <>
       <Table dataSource={props.workOrders} bordered rowKey="order_id" loading={props.loading} pagination={{ hideOnSinglePage: true }} expandable={{ expandedRowRender: record => <WorkOrderTableExpandable order={record} />, expandIcon: ({ expanded, onExpand, record }) => (expanded ? <DownOutlined onClick={e => onExpand(record, e)} /> : <RightOutlined onClick={e => onExpand(record, e)} />), columnTitle: '工单详情', columnWidth: '200px' }}>
-        <Table.Column width="200px" align="center" title="客户名称" dataIndex="customer_name" key="customer_name" />
-        <Table.Column width="200px" align="center" title="所属企业" dataIndex="customer_company" key="customer_company" />
+        <Table.Column
+          width="200px"
+          align="center"
+          title="客户信息"
+          key="customer_name"
+          render={(_, record: WorkOrder) => (
+            <>
+              {record.customer_name}
+              <br />
+              {record.customer_company}
+            </>
+          )}
+        />
         <Table.Column width="200px" align="center" title="产品名称" dataIndex="model_name" key="model_name" />
         <Table.Column width="150px" align="center" title="服务类型" dataIndex="order_type" key="order_type" render={(_, record: WorkOrder) => <>{services.find(v => v.id === record.order_type)?.text}</>} />
         <Table.Column width="250px" align="center" title="创建时间" dataIndex="create_time" key="create_time" />
